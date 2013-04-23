cc504c1705966f9327f0da238da6b3f71ac883dc
Elijah Insua <tmpvar@gmail.com>
2013-04-22 19:45:43 -0700

    initial version

---
+++ index.js
@@ -0,0 +1,54 @@
+var async = (typeof setImmediate === 'function') ?
+            setImmediate :
+            function(cb) { setTimeout(cb, 0); };
+
+function AnimationFrame(afterFrameFn) {
+  var frameRequests = [],
+      frameIdx = 0,
+      ticking = false,
+      last = 0;
+
+  var tick = function() {
+    console.log('tick')
+    if (frameRequests.length > 0) {
+
+      async(tick);
+      var now = Date.now();
+      if (now - last > 16) {
+        for (var i = 0; i<frameRequests.length; i++) {
+          frameRequests[i].cb(now);
+        }
+
+        last = now
+
+        afterFrameFn && afterFrameFn();
+      }
+    }
+
+    ticking = false;
+  };
+
+  this.cancelAnimationFrame = function(frameIdx) {
+    frameRequests = frameRequests.filter(function(a) {
+      return a.idx !== frameIdx;
+    });
+  }
+
+  this.requestAnimationFrame = function(cb) {
+    frameIdx++;
+
+    frameRequests.push({
+      idx : frameIdx,
+      cb :  cb
+    });
+
+    if (!ticking) {
+      ticking = true;
+      async(tick);
+    }
+
+    return frameIdx;
+  }
+}
+
+module.exports = AnimationFrame;
---
+++ test.js
@@ -0,0 +1,22 @@
+var AnimationFrame = require('./');
+var test = require('tap').test;
+
+test("give a callback at the end of a frame", function(t) {
+
+  var called = 0;
+  var manager = new AnimationFrame(function() {
+    called++;
+  });
+
+  var id = manager.requestAnimationFrame(function(time) {
+    t.equal(time, Date.now());
+    called++;
+
+    manager.cancelAnimationFrame(id);
+
+    setTimeout(function() {
+      t.equal(called, 2);
+      t.end();
+    }, 100);
+  });
+});
